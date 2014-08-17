c2267a68f9ee497e016bcbbcce2b25e8db62ad01
John Shiver <johneshiver@gmail.com>
2014-08-16 23:00:16 -0700

    Updated templates to include favicon, added static directory

--- blog.py
+++ blog.py
@@ -21,6 +21,16 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+# error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 # used to ensure dev server only started when app is
 # executed directly
 # change this for production
