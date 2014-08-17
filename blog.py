35a57ebf70f9f1a7c9141420859c8fac0effe58e
John Shiver <johneshiver@gmail.com>
2014-08-16 21:38:41 -0700

    Added templates

--- blog.py
+++ blog.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.script import Manager
 
 
 # create application instance
@@ -6,21 +7,22 @@ from flask import Flask
 # __name__ used to determine root path of app so that it can later
 # find resource files relative to location of app
 app = Flask(__name__)
+manager = Manager(app)
 
 
 # view functions
-@app.route('/')
+@app.route('/index')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,  %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 # used to ensure dev server only started when app is
 # executed directly
 # change this for production
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
