b7edbfa952adbafc459d885e07ad51599273bf77
John Shiver <johneshiver@gmail.com>
2014-08-17 21:36:13 -0700

    Added flask-wtf and some basic templates to index.html

--- blog.py
+++ blog.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 from datetime import datetime
 
 # create application instance
@@ -10,13 +16,26 @@ from datetime import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+# used by flask-wtf for csrf tokens
+# eventually move to env variable
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+# Forms
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 # view functions
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                            current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
@@ -28,7 +47,6 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def page_not_found(e):
