80466f8eee6045ddb9e880089f879f3de16407de
John Shiver <johneshiver@gmail.com>
2014-08-17 23:07:46 -0700

    Added db models

--- blog.py
+++ blog.py
@@ -1,24 +1,58 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 
+from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 from datetime import datetime
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # create application instance
 # web server passes all request to this object using WSGI
 # __name__ used to determine root path of app so that it can later
 # find resource files relative to location of app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 # used by flask-wtf for csrf tokens
 # eventually move to env variable
 app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+# models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 # Forms
@@ -58,4 +92,4 @@ def page_not_found(e):
 # executed directly
 # change this for production
 if __name__ == '__main__':
-    app.run()
+    manager.run()
